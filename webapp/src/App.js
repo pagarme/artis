a5c8f2a52cba16e08ad3b5c4298973249fb96832
Thiago M. Medeiros <thiagomunhozmedeiros@gmail.com>
2017-12-08 17:24:22 -0200

    pages: add initial pages structure
    - for customer, address and payment method pages

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,7 +1,42 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const App = () => (
-  <main>Here is where it all begins!</main>
-)
+import Content from './containers/Content'
+import Header from './components/Header'
+import Footer from './components/Footer'
+
+import style from './App.styles.css'
+
+class App extends Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      navigateTo: null,
+    }
+  }
+
+  navigateTo (navigateTo) {
+    this.setState({ navigateTo })
+  }
+
+  render () {
+    return (
+      <div className={style.checkout}>
+        <div className={style.wrapper}>
+          <Header
+            onPrev={this.navigateTo.bind(this, 'prev')}
+          />
+          <Content navigateTo={this.state.navigateTo} />
+          <Footer
+            total={33000.15}
+            buttonText={'Continuar'}
+            buttonClick={this.navigateTo.bind(this, 'next')}
+            companyName={'Pagar.me'}
+          />
+        </div>
+      </div>
+    )
+  }
+}
 
 export default App
