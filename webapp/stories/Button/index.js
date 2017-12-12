60330871edc9739422c9eb978f8d39242ef273be
Augusto Amaral <augusto.jm.amaral1@gmail.com>
2017-12-12 12:01:05 -0200

    stories: refactor button story
    Remove dark theme.

--- webapp/stories/Button/index.js
+++ webapp/stories/Button/index.js
@@ -11,47 +11,44 @@ import style from './style.css'
 storiesOf('Buttons', module)
   .add('All', () => (
     <div className={style.container}>
-      <div className={style.darkTheme}>
-        <h1>Dark Theme</h1>
-        <h2>Default Button</h2>
+      <h2>Default Button</h2>
 
-        <div className={style.spacing}>
-          <Button base="dark" relevance="high">High relevance</Button>
-        </div>
+      <div className={style.spacing}>
+        <Button base="dark" relevance="high">High relevance</Button>
+      </div>
 
-        <h2>Gradient</h2>
-        <div className={style.spacing}>
-          <Button base="dark" relevance="high" fill="gradient">Call to Action</Button>
-        </div>
+      <h2>Gradient</h2>
+      <div className={style.spacing}>
+        <Button base="dark" relevance="high" fill="gradient">Call to Action</Button>
+      </div>
 
-        <h2>Clean buttons with icons</h2>
+      <h2>Clean buttons with icons</h2>
 
-        <div className={style.spacing}>
-          <Button base="dark" relevance="normal" fill="clean"><BackIcon size={30} /></Button>
-          <Button base="dark" relevance="high" fill="clean"><CloseIcon size={30} /></Button>
-        </div>
+      <div className={style.spacing}>
+        <Button base="dark" relevance="normal" fill="clean"><BackIcon size={30} /></Button>
+        <Button base="dark" relevance="high" fill="clean"><CloseIcon size={30} /></Button>
+      </div>
 
-        <h2>Sizes</h2>
+      <h2>Sizes</h2>
 
-        <div className={style.spacing}>
-          <Button base="dark" size="extra-small" relevance="high">Extra-small</Button>
-        </div>
+      <div className={style.spacing}>
+        <Button base="dark" size="extra-small" relevance="high">Extra-small</Button>
+      </div>
 
-        <div className={style.spacing}>
-          <Button base="dark" size="small" relevance="high">Small</Button>
-        </div>
+      <div className={style.spacing}>
+        <Button base="dark" size="small" relevance="high">Small</Button>
+      </div>
 
-        <div className={style.spacing}>
-          <Button base="dark" relevance="high">Default</Button>
-        </div>
+      <div className={style.spacing}>
+        <Button base="dark" relevance="high">Default</Button>
+      </div>
 
-        <div className={style.spacing}>
-          <Button base="dark" size="large" relevance="high">Large</Button>
-        </div>
+      <div className={style.spacing}>
+        <Button base="dark" size="large" relevance="high">Large</Button>
+      </div>
 
-        <div className={style.spacing}>
-          <Button base="dark" size="extra-large" relevance="high">Extra-large</Button>
-        </div>
+      <div className={style.spacing}>
+        <Button base="dark" size="extra-large" relevance="high">Extra-large</Button>
       </div>
     </div>
   ))
