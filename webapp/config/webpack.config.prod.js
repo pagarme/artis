f7a1b78afaf94fec3d70fc99308e627ff4c375f0
Vitor Capretz <capretzvitor@gmail.com>
2017-12-12 13:58:54 -0200

    chore: remove autoprefixer
    postcss-next already have autoprefixer so we don't need it

--- webapp/config/webpack.config.prod.js
+++ webapp/config/webpack.config.prod.js
@@ -1,6 +1,5 @@
 'use strict';
 
-const autoprefixer = require('autoprefixer');
 const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
@@ -200,15 +199,6 @@ module.exports = {
                         ident: 'postcss',
                         plugins: () => [
                           require('postcss-flexbugs-fixes'),
-                          autoprefixer({
-                            browsers: [
-                              '>1%',
-                              'last 4 versions',
-                              'Firefox ESR',
-                              'not ie < 9', // React doesn't support IE8 anyway
-                            ],
-                            flexbox: 'no-2009',
-                          }),
                           require('postcss-import'),
                           require('postcss-sass-each'),
                           // This is necessary because postcss-url doesn't add
