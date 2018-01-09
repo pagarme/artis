a75dfd6e8cab5c9733ddcbe5e59192ce3bc94ad3
Vitor Capretz <capretzvitor@gmail.com>
2018-01-09 13:56:32 -0200

    chore: update tests for button component
    add a new test to check for disabled button

--- webapp/src/components/Button/Button.test.js
+++ webapp/src/components/Button/Button.test.js
@@ -4,16 +4,32 @@ import { mount } from 'enzyme'
 import Button from './index'
 
 describe('Button', () => {
-  const onClick = jest.fn()
-
-  const component = mount(
-    <Button onClick={onClick}>
-      Test
-    </Button>
-  )
-
   it('should be clicked', () => {
+    const onClick = jest.fn()
+
+    const component = mount(
+      <Button onClick={onClick}>
+        Test
+      </Button>
+    )
+
     component.simulate('click')
     expect(onClick).toHaveBeenCalled()
+  })
+
+  it('should not be clicked', () => {
+    const onClick = jest.fn()
+
+    const component = mount(
+      <Button
+        onClick={onClick}
+        disabled
+      >
+        Test
+      </Button>
+    )
+
+    component.simulate('click')
+    expect(onClick).not.toHaveBeenCalled()
   })
 })
