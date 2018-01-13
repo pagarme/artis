7134097da83516d4ea9b639b08ad995ab80134f3
Vitor Capretz <capretzvitor@gmail.com>
2018-01-13 20:00:55 -0200

    chore: move addressForm to containers folder
    it is a component that handles state, so it should be a container
    the theme prop was also added so it can have its own styling
    
    the states.js file, as it is used in more than one place, was moved to
    the helpers folder

--- webapp/src/theme-pagarme/index.js
+++ webapp/src/theme-pagarme/index.js
@@ -6,6 +6,7 @@ import UIHeader from './header/index.css'
 import UIInput from './input/index.css'
 import UIProgressBar from './progress-bar/index.css'
 import UICheckout from './checkout/index.css'
+import UIAddressForm from './address-form/index.css'
 
 export default {
   UIButton,
@@ -16,4 +17,5 @@ export default {
   UIInput,
   UIProgressBar,
   UICheckout,
+  UIAddressForm,
 }
