411eecd1992205d88c47e6eb13c2b57e8c0b8af7
Vitor Capretz <capretzvitor@gmail.com>
2018-01-19 16:46:10 -0200

    confirmation: add success page
    first implementation for success page, according to the zeplin link

--- webapp/src/pages/Confirmation.js
+++ webapp/src/pages/Confirmation.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { themr } from 'react-css-themr'
+import classNames from 'classnames'
 
 import { Grid, Row, Col } from '../components/Grid'
 import successIcon from '../images/success-icon.png'
@@ -8,63 +9,155 @@ import errorIcon from '../images/error-icon.png'
 
 const applyThemr = themr('UIConfirmationPage')
 
-const defaultColSize = 12
+const iconColSize = 4
+const contentColSize = 8
+const mediumColSize = 6
 
-const Confirmation = ({ success, theme }) => (
-  <Grid
-    className={theme.page}
-  >
-    <Row>
-      <Col
-        tv={4}
-        desk={4}
-        tablet={4}
-        className={theme.title}
-        alignCenter
-      >
-        <div className={theme.confirmationIcon}>
-          {success && <img
-            src={successIcon}
-            alt={'Ícone de sucesso'}
-            className={theme.successIcon}
-          />}
+const paymentInfo = {
+  name: 'Dan Abramov',
+  amount: 'R$ 1000.000,00',
+  method: 'MasterCard Black',
+  address: 'Rua Lorem Ipsum Consectetuer, 1001 - apartamento 101 Água Fria',
+  payment: '2x vezes sem juros com 20% de desconto na primeira parcela',
+}
 
-          {!success && <img
-            src={errorIcon}
-            alt={'Ícone de erro'}
-            className={theme.errorIcon}
-          />}
-        </div>
-      </Col>
-      <Col
-        tv={8}
-        desk={8}
-        tablet={8}
-        palm={defaultColSize}
-        className={theme.title}
+class Confirmation extends React.Component {
+  renderSuccessInfo () {
+    const { theme } = this.props
+
+    return (
+      <div>
+        <Row className={theme.title}>
+          <h4 className={theme.success}>
+            Seu pagamento foi concluído com sucesso
+          </h4>
+        </Row>
+        <Row>
+          <Col
+            tv={mediumColSize}
+            desk={mediumColSize}
+            tablet={mediumColSize}
+          >
+            <Row>
+              <div>
+                <div className={theme.field}>Nome</div>
+                <div className={theme.value}>{paymentInfo.name}</div>
+              </div>
+            </Row>
+            <Row>
+              <div>
+                <div className={theme.field}>Valor pago</div>
+                <div className={theme.value}>{paymentInfo.amount}</div>
+              </div>
+            </Row>
+            <Row>
+              <div>
+                <div className={theme.field}>Cartão</div>
+                <div className={theme.value}>{paymentInfo.method}</div>
+              </div>
+            </Row>
+          </Col>
+          <Col
+            tv={mediumColSize}
+            desk={mediumColSize}
+            tablet={mediumColSize}
+          >
+            <Row>
+              <div>
+                <div className={theme.field}>Endereço de entrega</div>
+                <div className={
+                  classNames(
+                    theme.value,
+                    theme.rightValue,
+                  )}
+                >{paymentInfo.address}</div>
+              </div>
+            </Row>
+            <Row>
+              <div>
+                <div className={theme.field}>Quantidade de parcelas</div>
+                <div className={
+                  classNames(
+                    theme.value,
+                    theme.rightValue,
+                  )}
+                >{paymentInfo.payment}</div>
+              </div>
+            </Row>
+          </Col>
+        </Row>
+      </div>
+    )
+  }
+
+  renderErrorInfo () {
+    const { theme } = this.props
+
+    return (
+      <div>
+        <Row className={theme.title}>
+          <p className={theme.error}>
+            Seu pagamento foi recusado
+          </p>
+          <p className={theme.info}>
+            Ocorreu um erro ao processar sua transação,
+            tente novamente mais tarde ou entre em contato com seu banco.
+          </p>
+        </Row>
+      </div>
+    )
+  }
+
+  render () {
+    const { theme, success } = this.props
+
+    return (
+      <Grid
+        className={theme.page}
       >
-        {success &&
-          <div>
-            <p className={theme.success}>
-              Seu pagamento foi concluído com sucesso
-            </p>
-          </div>
-        }
-        {!success &&
-          <div>
-            <p className={theme.error}>
-              Seu pagamento foi recusado
-            </p>
-            <p className={theme.info}>
-              Ocorreu um erro ao processar sua transação,
-              tente novamente mais tarde ou entre em contato com seu banco.
-            </p>
-          </div>
-        }
-      </Col>
-    </Row>
-  </Grid>
-)
+        <Row
+          stretch
+        >
+          <Col
+            tv={iconColSize}
+            desk={iconColSize}
+            tablet={iconColSize}
+            palm={iconColSize}
+            className={theme.title}
+            alignCenter
+          >
+            <div className={theme.confirmationIcon}>
+              {success && <img
+                src={successIcon}
+                alt={'Ícone de sucesso'}
+                className={theme.successIcon}
+              />}
+
+              {!success && <img
+                src={errorIcon}
+                alt={'Ícone de erro'}
+                className={theme.errorIcon}
+              />}
+            </div>
+          </Col>
+          <Col
+            tv={contentColSize}
+            desk={contentColSize}
+            tablet={contentColSize}
+            palm={contentColSize}
+          >
+            {success &&
+              this.renderSuccessInfo()
+            }
+            {!success &&
+              this.renderErrorInfo()
+            }
+          </Col>
+        </Row>
+      </Grid>
+    )
+  }
+}
 
 Confirmation.propTypes = {
   theme: PropTypes.shape({
