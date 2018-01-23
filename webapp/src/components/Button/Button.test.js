d735bbc32272e231a344d2fc59526312133e9c5e
Vitor Capretz <capretzvitor@gmail.com>
2018-01-23 18:46:49 -0200

    button: add tests for button

--- webapp/src/components/Button/Button.test.js
+++ webapp/src/components/Button/Button.test.js
@@ -32,4 +32,35 @@ describe('Button', () => {
     component.simulate('click')
     expect(onClick).not.toHaveBeenCalled()
   })
+
+  it('should have type reset', () => {
+    const onClick = jest.fn()
+
+    const component = mount(
+      <Button
+        onClick={onClick}
+        disabled
+        type={'reset'}
+      >
+        {'Click me like there\'s no tomorrow'}
+      </Button>
+    )
+
+    expect(component.prop('type')).toEqual('reset')
+  })
+
+  it('should have default value for fill', () => {
+    const onClick = jest.fn()
+
+    const component = mount(
+      <Button
+        onClick={onClick}
+        disabled
+      >
+        Action!!!!
+      </Button>
+    )
+
+    expect(component.prop('fill')).toEqual('flat')
+  })
 })
