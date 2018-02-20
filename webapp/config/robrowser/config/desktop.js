44bdf5342f169a801db2c24fa4aea33c0ea843c3
Thiago M. Medeiros <thiagomunhozmedeiros@gmail.com>
2018-02-20 11:41:30 -0300

    robrowser: add devices config

--- webapp/config/robrowser/config/desktop.js
+++ webapp/config/robrowser/config/desktop.js
@@ -4,10 +4,62 @@ module.exports = [
     os_version: '7',
     local: true,
     resolution: '1920x1080',
-    browsers: [
+    browsers: [      
+      {
+        browserName: 'ie',
+        browser_version: '10.0',
+      },
+      {
+        browserName: 'firefox',
+        browser_version: '30.0',
+      },
       {
         browserName: 'chrome',
-        browser_version: '62.0',
+        browser_version: '40.0',
+      },
+    ],
+  },
+  {
+    os: 'windows',
+    os_version: '8',
+    local: true,
+    resolution: '1920x1080',
+    browsers: [
+      {
+        browserName: 'ie',
+        browser_version: '10.0',
+      },
+      {
+        browserName: 'chrome',
+        browser_version: '40.0',
+      },
+      {
+        browserName: 'firefox',
+        browser_version: '30.0',
+      },
+    ],
+  },
+  {
+    os: 'windows',
+    os_version: '10',
+    local: true,
+    resolution: '1920x1080',
+    browsers: [
+      {
+        browserName: 'ie',
+        browser_version: '11.0',
+      },
+      {
+        browserName: 'edge',
+        browser_version: '14.0',
+      },
+      {
+        browserName: 'chrome',
+        browser_version: '40.0',
+      },
+      {
+        browserName: 'firefox',
+        browser_version: '40.0',
       },
     ],
   },
@@ -18,12 +70,20 @@ module.exports = [
     resolution: '1920x1080',
     browsers: [
       {
-        browserName: 'firefox',
-        browser_version: '38.0',
+        browserName: 'safari',
+        browser_version: '10.1',
       },
       {
         browserName: 'chrome',
-        browser_version: '55.0',
+        browser_version: '40.0',
+      },
+      {
+        browserName: 'firefox',
+        browser_version: '40.0',
+      },
+      {
+        browserName: 'opera',
+        browser_version: '12.15',
       },
     ],
   },
