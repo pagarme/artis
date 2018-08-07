730a08d3cace7c2c7b3c6d2aaa76a3564cd3d86f
Allan Ramos <allangrds@gmail.com>
2018-08-07 12:55:35 -0300

    cypress: add shipping back case to flow test
    Tests using the case when api don't
    contain shipping.

--- cypress/integration/custom/flow/back.spec.js
+++ cypress/integration/custom/flow/back.spec.js
@@ -307,6 +307,105 @@ describe('Pagarme', () => {
             })
           })
         })
+        describe('When Shipping do not exist', () => {
+          const api = `
+            const key = 'ek_test_sjQXl3mVUFu1QQYpiSvUBaybtXtXjz';
+
+            const transaction = {
+              amount: 10000,
+            }
+
+            const customer = {
+              name: 'Dan Abramov',
+              documentNumber: '19981596639',
+              email: 'mercurio@pagar.me',
+              phoneNumber: '1130442277',
+              allowedDocuments: ['CPF', 'CNPJ'],
+            }
+
+            const billing = {
+              street: 'Rua Fidêncio Ramos',
+              number: '308',
+              additionalInfo: 'Pagar.me',
+              neighborhood: 'Vila Olimpia',
+              city: 'São Paulo',
+              state: 'SP',
+              zipcode: '04551010',
+            }
+
+            const checkout = createCheckout({
+              key,
+              transaction,
+              customer,
+            })
+
+            checkout
+              .open()
+            `
+
+          it('will return from payment to shipping', () => {
+            openCustomWithParams(api)
+            fillAddressPage()
+
+            cy.get('.switch__switch').click()
+            cy.get('[type="submit"]:last').click()
+
+            fillAddressPage('03677030', '456')
+
+            cy.get('[type="submit"]:last').click()
+
+            cy.get('.navigation-bar__buttonContainer > button:first').click()
+
+            cy.get('.addressesPage__title').should('exist')
+            cy.get('.addressesPage__title').then((elem) => {
+              expect(elem[0].innerText).to.eq('Onde devemos fazer a entrega?')
+            })
+          })
+          it('will return from payment to billing', () => {
+            openCustomWithParams(api)
+            fillAddressPage()
+
+            cy.get('[type="submit"]:last').click()
+            cy.get('.navigation-bar__buttonContainer > button:first').click()
+
+            cy.get('.addressesPage__title').should('exist')
+            cy.get('.addressesPage__title').then((elem) => {
+              expect(elem[0].innerText).to.eq('Qual é seu endereço de cobrança?')
+            })
+          })
+          it('will return from boleto to payment', () => {
+            openCustomWithParams(api)
+            fillAddressPage()
+
+            cy.get('[type="submit"]:last').click()
+
+            cy.get('.paymentoptionsPage__optionsContainer > .actionButton__wrapper:last').click()
+
+            cy.get('[type="submit"]:last').click()
+            cy.get('.navigation-bar__buttonContainer > button:first').click()
+
+            cy.get('.paymentoptionsPage__title').should('exist')
+            cy.get('.paymentoptionsPage__title').then((elem) => {
+              expect(elem[0].innerText).to.eq('Como quer pagar?')
+            })
+          })
+          it('will return from credit card to payment', () => {
+            openCustomWithParams(api)
+            fillAddressPage()
+
+            cy.get('[type="submit"]:last').click()
+
+            cy.get('.paymentoptionsPage__optionsContainer > .actionButton__wrapper:first').click()
+
+            cy.get('[type="submit"]:last').click()
+            cy.get('.navigation-bar__buttonContainer > button:first').click()
+
+            cy.get('.paymentoptionsPage__title').should('exist')
+            cy.get('.paymentoptionsPage__title').then((elem) => {
+              expect(elem[0].innerText).to.eq('Como quer pagar?')
+            })
+          })
+        })
       })
     })
   })
