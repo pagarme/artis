e33b91efec1f833742c75e5f30e02e904ea41763
Allan Ramos <allangrds@gmail.com>
2018-08-07 13:15:57 -0300

    cypress: add full api test case on flow back tests
    Tests using the case when api
    contain customer,billing and shipping.

--- cypress/integration/custom/flow/back.spec.js
+++ cypress/integration/custom/flow/back.spec.js
@@ -406,6 +406,82 @@ describe('Pagarme', () => {
             })
           })
         })
+        describe('When Customer, Billing and Shipping exist', () => {
+          const api = `
+            const key = 'ek_test_sjQXl3mVUFu1QQYpiSvUBaybtXtXjz';
+
+            const transaction = {
+              amount: 10000,
+            }
+
+            const customer = {
+              name: 'Dan Abramov',
+              documentNumber: '19981596639',
+              email: 'mercurio@pagar.me',
+              phoneNumber: '1130442277',
+              allowedDocuments: ['CPF', 'CNPJ'],
+            }
+
+            const billing = {
+              street: 'Rua Fidêncio Ramos',
+              number: '308',
+              additionalInfo: 'Pagar.me',
+              neighborhood: 'Vila Olimpia',
+              city: 'São Paulo',
+              state: 'SP',
+              zipcode: '04551010',
+            }
+
+            const shipping = {
+              street: 'Rua Fidêncio Ramos',
+              number: '308',
+              additionalInfo: 'Pagar.me',
+              neighborhood: 'Vila Olimpia',
+              city: 'São Paulo',
+              state: 'SP',
+              zipcode: '04551010',
+              fee: 1599,
+            }
+
+            const checkout = createCheckout({
+              key,
+              transaction,
+              customer,
+              billing,
+              shipping,
+            })
+
+            checkout
+              .open()
+            `
+
+          it('will return from boleto to payment', () => {
+            openCustomWithParams(api)
+
+            cy.get('.paymentoptionsPage__optionsContainer > .actionButton__wrapper:last').click()
+
+            cy.get('[type="submit"]:last').click()
+            cy.get('.navigation-bar__buttonContainer > button:first').click()
+
+            cy.get('.paymentoptionsPage__title').should('exist')
+            cy.get('.paymentoptionsPage__title').then((elem) => {
+              expect(elem[0].innerText).to.eq('Como quer pagar?')
+            })
+          })
+          it('will return from credit card to payment', () => {
+            openCustomWithParams(api)
+
+            cy.get('.paymentoptionsPage__optionsContainer > .actionButton__wrapper:first').click()
+
+            cy.get('[type="submit"]:last').click()
+            cy.get('.navigation-bar__buttonContainer > button:first').click()
+
+            cy.get('.paymentoptionsPage__title').should('exist')
+            cy.get('.paymentoptionsPage__title').then((elem) => {
+              expect(elem[0].innerText).to.eq('Como quer pagar?')
+            })
+          })
+        })
       })
     })
   })
