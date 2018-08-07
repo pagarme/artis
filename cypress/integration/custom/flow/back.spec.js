dbb64d8f36603c02350250ca2e72310151d3cffa
Allan Ramos <allangrds@gmail.com>
2018-08-07 12:41:53 -0300

    cypress: add second back case to flow test
    Tests using the case when api does'nt
    contain billing and shipping.

--- cypress/integration/custom/flow/back.spec.js
+++ cypress/integration/custom/flow/back.spec.js
@@ -204,6 +204,109 @@ describe('Pagarme', () => {
             })
           })
         })
+        describe('When Billing and Shipping does not exist', () => {
+          const api = `
+            const key = 'ek_test_sjQXl3mVUFu1QQYpiSvUBaybtXtXjz';
+
+            const transaction = {
+              amount: 10000,
+            }
+
+            const customer = {
+              name: 'Dan Abramov',
+              documentNumber: '19981596639',
+              email: 'mercurio@pagar.me',
+              phoneNumber: '1130442277',
+              allowedDocuments: ['CPF', 'CNPJ'],
+            }
+
+            const checkout = createCheckout({
+              key,
+              transaction,
+              customer,
+            })
+
+            checkout
+              .open()
+            `
+
+          it('will return from shipping to billing', () => {
+            openCustomWithParams(api)
+
+            fillAddressPage()
+
+            cy.get('.switch__switch').click()
+            cy.get('[type="submit"]:last').click()
+            cy.get('.navigation-bar__buttonContainer > button:first').click()
+
+            cy.get('.addressesPage__title').should('exist')
+            cy.get('.addressesPage__title').then((elem) => {
+              expect(elem[0].innerText).to.eq('Qual é seu endereço de cobrança?')
+            })
+          })
+          it('will return from payment to shipping', () => {
+            openCustomWithParams(api)
+            fillAddressPage()
+
+            cy.get('.switch__switch').click()
+            cy.get('[type="submit"]:last').click()
+
+            fillAddressPage('03677030', '456')
+
+            cy.get('[type="submit"]:last').click()
+
+            cy.get('.navigation-bar__buttonContainer > button:first').click()
+
+            cy.get('.addressesPage__title').should('exist')
+            cy.get('.addressesPage__title').then((elem) => {
+              expect(elem[0].innerText).to.eq('Onde devemos fazer a entrega?')
+            })
+          })
+          it('will return from payment to billing', () => {
+            openCustomWithParams(api)
+            fillAddressPage()
+
+            cy.get('[type="submit"]:last').click()
+            cy.get('.navigation-bar__buttonContainer > button:first').click()
+
+            cy.get('.addressesPage__title').should('exist')
+            cy.get('.addressesPage__title').then((elem) => {
+              expect(elem[0].innerText).to.eq('Qual é seu endereço de cobrança?')
+            })
+          })
+          it('will return from boleto to payment', () => {
+            openCustomWithParams(api)
+            fillAddressPage()
+
+            cy.get('[type="submit"]:last').click()
+
+            cy.get('.paymentoptionsPage__optionsContainer > .actionButton__wrapper:last').click()
+
+            cy.get('[type="submit"]:last').click()
+            cy.get('.navigation-bar__buttonContainer > button:first').click()
+
+            cy.get('.paymentoptionsPage__title').should('exist')
+            cy.get('.paymentoptionsPage__title').then((elem) => {
+              expect(elem[0].innerText).to.eq('Como quer pagar?')
+            })
+          })
+          it('will return from credit card to payment', () => {
+            openCustomWithParams(api)
+            fillAddressPage()
+
+            cy.get('[type="submit"]:last').click()
+
+            cy.get('.paymentoptionsPage__optionsContainer > .actionButton__wrapper:first').click()
+
+            cy.get('[type="submit"]:last').click()
+            cy.get('.navigation-bar__buttonContainer > button:first').click()
+
+            cy.get('.paymentoptionsPage__title').should('exist')
+            cy.get('.paymentoptionsPage__title').then((elem) => {
+              expect(elem[0].innerText).to.eq('Como quer pagar?')
+            })
+          })
+        })
       })
     })
   })
