906b1034171fe8f6d9adefd460059370f8ea2aac
Augusto José Miranda Amaral <augusto.jm.amaral1@gmail.com>
2018-10-04 18:47:12 -0300

    chore: remove unused robrowser

--- config/robrowser/auth.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  host: 'hub.browserstack.com',
-  port: 80,
-  user: '',
-  pwd: '',
-}
--- config/robrowser/index.js
+++
@@ -1,9 +0,0 @@
-const createTestTo = require('./createTestTo')
-const desktop = require('./config/desktop')
-const mobile = require('./config/mobile')
-
-module.exports = [
-  ...createTestTo('desktop', desktop),
-  ...createTestTo('mobile', mobile),
-]
-
